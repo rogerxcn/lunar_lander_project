b9633a98b00992cb5faf7e4565c16cadf0c93c5a
rogerx <mail@rogerx.me>
2019-11-10 13:45:18 -0800

    initial api

---
+++ api.py
@@ -0,0 +1,17 @@
+def entire_map()
+
+def local_map(width, height)
+
+def travel_distance()
+
+def num_turns()
+
+def current_episode()
+
+def agent_location()
+
+def available_actions()
+
+def remaining_nodes()
+
+def num_unvisited_nodes()
