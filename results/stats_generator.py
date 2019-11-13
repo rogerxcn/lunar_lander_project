dcd8a8c747e31ae9ec2acc9181e7a4e639321128
Roger Xin <6767601+rogerxcn@users.noreply.github.com>
2019-11-13 15:45:49 -0800

    add naive sarsa stats

--- results/stats_generator.py
+++ results/stats_generator.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 
 fn1 = "sarsa_reward.txt"
 fn2 = "random_reward.txt"
-fn3 = "sarsa_reward_1.txt"
+fn4 = "naive_sarsa_reward.txt"
 
 x_length = 10000
 
 g1 = np.loadtxt(fn1)
 g2 = np.loadtxt(fn2)
-g3 = np.loadtxt(fn3)
+g4 = np.loadtxt(fn4)
 
 x = np.linspace(0, x_length, g1.shape[0]-1)
 
-plt.plot(x, g1[1:], label='Sarsa (attempt 1)', color="blue")
-plt.plot(x, g3[1:], label='Sarsa (attempt 2)', color="green")
+plt.plot(x, g1[1:], label='Sarsa (optimized)', color="green")
+plt.plot(x, g4[1:], label='Sarsa (naive)', color="blue")
 plt.plot(x, g2[1:], label='Random', color="grey")
 plt.legend()
 plt.title("Acquired Rewards")
